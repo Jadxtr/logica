c12b3ca22abc6b03257973799a403bac73067dc4
instrutor-edmar <ssg.s24ultra@gmail.com>
2026-02-27 20:37:56 +0000

    Resolução da questão 31

--- Exercicio31.java
+++ Exercicio31.java
@@ -2,11 +2,20 @@
 
 import java.util.Scanner;
 
-public class Exercicio31 {
+public class Exercicio31 {    
+	public static int fibonacci (int numero){
+        if(numero == 0) return 1;
+		if(numero == 1) return 2;
+        return fibonacci(numero-1) + fibonacci(numero-2);
+    }
+    
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in); 
 
-		//escreva seu código aqui
+		System.out.print("Informe um termo para saber a sua posição na sequencia Fibonacci: ");
+		int num = input.nextInt();
+		
+		System.out.println("O " + num + "º termo da sequência Fibonacci é: "+ fibonacci(num));
 		
 		input.close();
 	}
